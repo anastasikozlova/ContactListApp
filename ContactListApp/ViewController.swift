a5968f468f8888fc611be10d7c16b6dd2c850309
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-07-09 14:24:29 +0200

    Initial Commit

---
+++ ContactListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListApp
+//
+//  Created by Анастасия Козлова on 09.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
